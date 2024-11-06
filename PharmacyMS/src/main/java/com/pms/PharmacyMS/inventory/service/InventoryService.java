4fcf57526a5553143ac2aa70993c55f1eb566773
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2024-11-06 18:45:38 +0530

    update

--- PharmacyMS/src/main/java/com/pms/PharmacyMS/inventory/service/InventoryService.java
+++ PharmacyMS/src/main/java/com/pms/PharmacyMS/inventory/service/InventoryService.java
@@ -9,7 +9,9 @@ import com.pms.PharmacyMS.inventory.repository.InventoryRepo;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -34,6 +36,16 @@ public class InventoryService {
     public List<InventoryViewDto> getAllInventoryItems() {
             return inventoryRepo.findAllInventoryView();
     }
+
+    //get the inventory by id
+    public InventoryDto getInventoryById(int id) {
+        // Fetch the inventory from the database by ID
+        Inventory inventory = inventoryRepo.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Inventory not found"));
+
+        return modelMapper.map(inventory, InventoryDto.class);
+    }
+
 
     // Update inventory
     public InventoryDto updateInventory(int id, InventoryDto inventoryDto) {
