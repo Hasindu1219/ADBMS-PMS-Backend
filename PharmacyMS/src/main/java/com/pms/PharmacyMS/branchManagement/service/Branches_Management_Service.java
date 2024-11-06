5c6449effa14f6370c0bb2f23078d3e92fda27ae
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-11-06 13:06:06 +0530

    Update Branches_Management_Service.java

--- PharmacyMS/src/main/java/com/pms/PharmacyMS/branchManagement/service/Branches_Management_Service.java
+++ PharmacyMS/src/main/java/com/pms/PharmacyMS/branchManagement/service/Branches_Management_Service.java
@@ -2,6 +2,7 @@ package com.pms.PharmacyMS.branchManagement.service;
 
 import com.pms.PharmacyMS.branchManagement.dto.Branches_View_Dto;
 import com.pms.PharmacyMS.branchManagement.entity.Branches_View_Entity;
+import com.pms.PharmacyMS.branchManagement.repository.BranchRepo;
 import com.pms.PharmacyMS.branchManagement.repository.Branches_View_Repo;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
@@ -19,11 +20,23 @@ public class Branches_Management_Service {
     @Autowired
     private Branches_View_Repo branchesViewRepo;
     @Autowired
+    private BranchRepo branchRepo;
+    @Autowired
     private ModelMapper modelMapper;
 
+
+
+    // -------------------------------- Service for Branches_view -------------------START
 
     public List<Branches_View_Dto> getAllBranches (){
         List<Branches_View_Entity> allBranches = branchesViewRepo.findAll();
         return modelMapper.map(allBranches, new TypeToken <ArrayList<Branches_View_Dto>>(){}.getType());
     }
+
+    // -------------------------------- Service for Branches_view -------------------END
+
+    // -------------------------------- Service for Branches TABLE -------------------START
+
+
+    // -------------------------------- Service for Branches TABLE -------------------END
 }
