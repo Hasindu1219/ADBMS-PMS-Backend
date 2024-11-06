dc31633e4c3343a22315675a28b484a08982bf26
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-11-06 11:16:52 +0530

    Add branch_view function

---
+++ PharmacyMS/src/main/java/com/pms/PharmacyMS/branchManagement/dto/Branches_View_Dto.java
@@ -0,0 +1,17 @@
+package com.pms.PharmacyMS.branchManagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Branches_View_Dto {
+    private int branch_id;
+    private String branch_name;
+    private String location;
+    private String contact_number;
+    private String manager_name;
+    private String user_id;
+}
---
+++ PharmacyMS/src/main/java/com/pms/PharmacyMS/branchManagement/service/Branches_Management_Service.java
@@ -0,0 +1,29 @@
+package com.pms.PharmacyMS.branchManagement.service;
+
+import com.pms.PharmacyMS.branchManagement.dto.Branches_View_Dto;
+import com.pms.PharmacyMS.branchManagement.entity.Branches_View_Entity;
+import com.pms.PharmacyMS.branchManagement.repository.Branches_View_Repo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class Branches_Management_Service {
+
+    @Autowired
+    private Branches_View_Repo branchesViewRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public List<Branches_View_Dto> getAllBranches (){
+        List<Branches_View_Entity> allBranches = branchesViewRepo.findAll();
+        return modelMapper.map(allBranches, new TypeToken <ArrayList<Branches_View_Dto>>(){}.getType());
+    }
+}
